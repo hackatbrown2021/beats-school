971ba86de76e0098a31121b9999209fa1f3c6300
Lucien Gaitskell <lucieng@gaitskell.com>
2021-01-23 17:22:34 -0500

    Add bad landing page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignIn from "./visly/Components/Navbar/SignIn";
 import Account from "./visly/Components/Navbar/Account";
 import Navbar from "./visly/Layout/Navbar";
 import LoginPage from "./Pages/Login";
+import LandingPage from "./Pages/Landing";
 import MusicApp from "./MusicApp.js";
 import { Route, Switch } from "react-router-dom";
 
@@ -16,7 +17,8 @@ function App() {
         AccountButton={authenticated ? <Account /> : <SignIn />}
       />
       <Switch>
-        <Route path="/" component={MusicApp} exact />
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/music" component={MusicApp} />
         <Route path="/login" component={LoginPage} />
       </Switch>
     </main>
---
+++ src/Pages/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Landing from "../visly/Layout/Landing";
+
+export default () => {
+  return (
+    <div style={{ width: "100%", justifyContent: "center", display: "flex" }}>
+      <Landing />
+    </div>
+  );
+};
