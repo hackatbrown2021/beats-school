95bc3006661e67b802421a69e35b513646a1b3f1
Lucien Gaitskell <lucieng@gaitskell.com>
2021-01-23 21:06:57 -0500

    Center align navbar

--- src/visly/Layout/Navbar.js
+++ src/visly/Layout/Navbar.js
@@ -84,82 +84,94 @@ function Navbar(_props) {
         activeVariants: activeVariants,
       }}
     >
-      {(getStyle) => [
-        props.Logo === undefined ? (
-          <Logo
-            key={"Buj9FHLzmF"}
-            {...getCompositeDefaultProps("Buj9FHLzmF")}
-            id="Logo_Buj9FHLzmF"
-            className="__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_Buj9FHLzmF"
-          />
-        ) : (
-          <LogoContext.Provider
-            key="Buj9FHLzmF-provider"
-            value={{
-              key: "Buj9FHLzmF",
-              id: "Logo_Buj9FHLzmF",
-              className:
-                "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_Buj9FHLzmF",
-              ...getCompositeDefaultProps("Buj9FHLzmF"),
-            }}
+      {(getStyle) => (
+        <>
+          <ContainerPrimitive
+            id={"Left_B1pg6BPk2P"}
+            className={
+              "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_B1pg6BPk2P"
+            }
+            key={"B1pg6BPk2P"}
+            addSpacing={false}
+            element={getStyle("B1pg6BPk2P", "htmlElement")}
           >
-            {props.Logo}
-          </LogoContext.Provider>
-        ),
-        <ContainerPrimitive
-          id={"NavLinks_8ruEmV82ch"}
-          className={
-            "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_8ruEmV82ch"
-          }
-          key={"8ruEmV82ch"}
-          addSpacing={false}
-          element={getStyle("8ruEmV82ch", "htmlElement")}
-        >
-          {props.DashboardButton === undefined ? (
-            <DashboardButton
-              key={"MrtQW2JAos"}
-              {...getCompositeDefaultProps("MrtQW2JAos")}
-              id="DashboardButton_MrtQW2JAos"
-              className="__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_MrtQW2JAos"
-            />
-          ) : (
-            <DashboardButtonContext.Provider
-              key="MrtQW2JAos-provider"
-              value={{
-                key: "MrtQW2JAos",
-                id: "DashboardButton_MrtQW2JAos",
-                className:
-                  "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_MrtQW2JAos",
-                ...getCompositeDefaultProps("MrtQW2JAos"),
-              }}
-            >
-              {props.DashboardButton}
-            </DashboardButtonContext.Provider>
-          )}
-        </ContainerPrimitive>,
-        <ContainerPrimitive
-          id={"Container_2MCpoSa1cQ"}
-          className={
-            "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_2MCpoSa1cQ"
-          }
-          key={"2MCpoSa1cQ"}
-          addSpacing={false}
-          element={getStyle("2MCpoSa1cQ", "htmlElement")}
-        >
-          {
-            <ContainerPrimitive
-              key={"4fTk9XDbJH"}
-              id={"Slot_4fTk9XDbJH"}
-              className={
-                "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_4fTk9XDbJH"
-              }
-              addSpacing={false}
-            >
-              {props.AccountButton}
-            </ContainerPrimitive>
-          }
-        </ContainerPrimitive>,
-      ]}
+            {props.Logo === undefined ? (
+              <Logo
+                key={"Buj9FHLzmF"}
+                {...getCompositeDefaultProps("Buj9FHLzmF")}
+                id="Logo_Buj9FHLzmF"
+                className="__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_Buj9FHLzmF"
+              />
+            ) : (
+              <LogoContext.Provider
+                key="Buj9FHLzmF-provider"
+                value={{
+                  key: "Buj9FHLzmF",
+                  id: "Logo_Buj9FHLzmF",
+                  className:
+                    "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_Buj9FHLzmF",
+                  ...getCompositeDefaultProps("Buj9FHLzmF"),
+                }}
+              >
+                {props.Logo}
+              </LogoContext.Provider>
+            )}
+          </ContainerPrimitive>
+          <ContainerPrimitive
+            id={"NavLinks_8ruEmV82ch"}
+            className={
+              "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_8ruEmV82ch"
+            }
+            key={"8ruEmV82ch"}
+            addSpacing={false}
+            element={getStyle("8ruEmV82ch", "htmlElement")}
+          >
+            {props.DashboardButton === undefined ? (
+              <DashboardButton
+                key={"MrtQW2JAos"}
+                {...getCompositeDefaultProps("MrtQW2JAos")}
+                id="DashboardButton_MrtQW2JAos"
+                className="__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_MrtQW2JAos"
+              />
+            ) : (
+              <DashboardButtonContext.Provider
+                key="MrtQW2JAos-provider"
+                value={{
+                  key: "MrtQW2JAos",
+                  id: "DashboardButton_MrtQW2JAos",
+                  className:
+                    "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_MrtQW2JAos",
+                  ...getCompositeDefaultProps("MrtQW2JAos"),
+                }}
+              >
+                {props.DashboardButton}
+              </DashboardButtonContext.Provider>
+            )}
+          </ContainerPrimitive>
+          <ContainerPrimitive
+            id={"Container_2MCpoSa1cQ"}
+            className={
+              "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_2MCpoSa1cQ"
+            }
+            key={"2MCpoSa1cQ"}
+            addSpacing={false}
+            element={getStyle("2MCpoSa1cQ", "htmlElement")}
+          >
+            {
+              <ContainerPrimitive
+                key={"4fTk9XDbJH"}
+                id={"Slot_4fTk9XDbJH"}
+                className={
+                  "__visly_reset_7X7HTLRqyD __visly_scope_49hJ88cQKh_4fTk9XDbJH"
+                }
+                addSpacing={false}
+              >
+                {props.AccountButton}
+              </ContainerPrimitive>
+            }
+          </ContainerPrimitive>
+        </>
+      )}
     </RootPrimitive>
   );
 }
